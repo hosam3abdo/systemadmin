130694fdcd497a67ede76180a77226e882983bdd
Hossam Abdo <66098544+hosam3abdo@users.noreply.github.com>
2024-01-13 19:31:16 +0200

    Add files via upload

--- create.blade.php
+++ create.blade.php
@@ -1,32 +1,38 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <title>Add Product</title>
-</head>
-<body>
-    <h1>Add Product</h1>
+@extends('Layout.dashboard')
+@section('title', 'Add product')
+@section('content')
+    <div class="col-12">
+        <form method="POST" action="{{ route('products.store') }}">
+            @csrf
+            <div class="card-body">
+                <div class="form-row">
+                    <div class="col-6">
+                        <label for="exampleInputEmail1">Name </label>
+                        <input type="text" name="name" class="form-control" id="name" placeholder="Name">
 
-    <form method="POST" action="{{ route('products.store') }}">
-        @csrf
-        <div class="col-6">
-            <label for="exampleInputEmail1">Name </label>
-            <input type="text" name="name" class="form-control" id="exampleInputEmail1" placeholder="Name ">
+                    </div>
+                    <div class="col-6">
+                        <label for="description">Description </label>
+                        <textarea name="description" id="description" cols="30" rows="10" class="form-control" placeholder="Name"></textarea>
+                    </div>
+                   
+                    <div class="col-6">
+                        <label for="exampleInputEmail3">Price</label>
+                        <input type="number" name="price" class="form-control" id="exampleInputEmail3"
+                            placeholder="Price">
+
+                    </div>
+
+
+                </div>
+
+            </div>
+            <div class="card-footer">
+                <button type="submit" class="btn btn-primary">create</button>
+                
+            </div>
+            
+        </form>
+    </div>
+    @endsection
            
-        </div>
-
-        <div>
-            <label for="description">Description:</label>
-            <textarea id="description" name="description" required></textarea>
-        </div>
-
-        <div>
-            <label for="price">Price:</label>
-            <input type="number" id="price" name="price" required>
-        </div>
-
-        <div>
-            <button type="submit">Add Product</button>
-        </div>
-    </form>
-</body>
-</html>
--- index.blade.php
+++ index.blade.php
@@ -1,26 +1,52 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <title>Product Listing</title>
-</head>
-<body>
-    <h1>Product Listing</h1>
 
-    <a href="{{ route('products.create') }}">Add Product</a>
-
-    <table>
+@extends('Layout.dashboard')
+@section('title','Allproducts')
+@section('css')
+<link rel="stylesheet" href="{{url('plugins/datatables-bs4/css/dataTables.bootstrap4.min.css')}}">
+  <link rel="stylesheet" href="{{url('plugins/datatables-responsive/css/responsive.bootstrap4.min.css')}}">
+  <link rel="stylesheet" href="{{url('plugins/datatables-buttons/css/buttons.bootstrap4.min.css')}}">
+@endsection
+@section('content')
+<div class="col-12">
+<table id="example1" class="table table-bordered table-striped">
+    <thead>
         <tr>
             <th>Name</th>
             <th>Description</th>
             <th>Price</th>
         </tr>
+    </thead>
+    <tbody>
         @foreach ($products as $product)
-            <tr>
-                <td>{{ $product->name }}</td>
-                <td>{{ $product->description }}</td>
-                <td>{{ $product->price }}</td>
-            </tr>
-        @endforeach
-    </table>
-</body>
-</html>
+        <tr>
+            <td>{{ $product->name }}</td>
+            <td>{{ $product->description }}</td>
+            <td>{{ $product->price }}</td>
+        </tr>
+    @endforeach
+  </table>
+</div>
+@endsection
+@section('js')
+<script src="{{url('plugins/datatables/jquery.dataTables.min.js')}}"></script>
+<script src="{{url('plugins/datatables-bs4/js/dataTables.bootstrap4.min.js')}}"></script>
+<script src="{{url('plugins/datatables-responsive/js/dataTables.responsive.min.js')}}"></script>
+<script src="{{url('plugins/datatables-responsive/js/responsive.bootstrap4.min.js')}}"></script>
+<script src="{{url('plugins/datatables-buttons/js/dataTables.buttons.min.js')}}"></script>
+<script src="{{url('plugins/datatables-buttons/js/buttons.bootstrap4.min.js')}}"></script>
+<script src="{{url('plugins/jszip/jszip.min.js')}}"></script>
+<script src="{{url('plugins/pdfmake/pdfmake.min.js')}}"></script>
+<script src="{{url('plugins/pdfmake/vfs_fonts.js')}}"></script>
+<script src="{{url('plugins/datatables-buttons/js/buttons.html5.min.js')}}"></script>
+<script src="{{url('plugins/datatables-buttons/js/buttons.print.min.js')}}"></script>
+<script src="{{url('plugins/datatables-buttons/js/buttons.colVis.min.js')}}"></script>
+<script>
+    $(function () {
+      $("#example1").DataTable({
+        "responsive": true, "lengthChange": false, "autoWidth": false,
+        "buttons": ["copy", "csv", "excel", "pdf", "print", "colvis"]
+      }).buttons().container().appendTo('#example1_wrapper .col-md-6:eq(0)');
+
+    });
+  </script>
+@endsection
